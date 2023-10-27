52b19cc3919bb3d7ea944030e1e42e8aa32cf420
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2023-10-27 12:24:43 +0200

    Create Conflict.java

---
+++ src/main/java/org/example/Conflict.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Conflict
+{
+
+
+
+    public Conflict()
+    {
+
+
+    }
+
+    public void meregeConfilct()
+    {
+        System.out.println("Sander");
+
+    }
+}
